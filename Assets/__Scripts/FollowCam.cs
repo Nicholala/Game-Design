b34b151180148b72fa728c831140f84e5317e1c1
Nicholala <835974413@qq.com>
2020-09-16 21:14:37 +0800

    Alpha

---
+++ Assets/__Scripts/FollowCam.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+    [Header("Set Dynamically")]
+    public float camZ;
+    void Awake()
+    {
+        //Transform CameraTrans = transform.Find("Main Camera");
+        //Camera = CameraTrans.gameObject;
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+       // if (POI == null) return;
+
+        //获取兴趣点的位置
+        //Vector3 destination = POI.transform.position;
+        Vector3 destination;
+        //如果兴趣点不存在，返回到P:[0,0,0]
+        if(POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            //获取兴趣点的位置
+            destination = POI.transform.position;
+            //如果兴趣点是一个projectile实例，检查它是否已经停止
+            if (POI.tag == "Projectile")
+            {
+                if(POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    //返回到默认视图
+                    POI = null;
+                    //在下一次时更新
+                    return;
+                }
+            }
+        }
+        //限定x和y的最小值
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //在摄像机当前位置和目标位置之间插值
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //保持destination.z的值为camZ,使摄像机距离足够远
+        destination.z = camZ;
+        //设置摄像机到目标位置
+        transform.position = destination;
+        //设置摄像机的投影面积，使地面始终处于画面中
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/__Scripts/Slingshot.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    static public Slingshot S;
+
+    //在Unity检视面板设置的字段
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+
+    //动态设置的字段
+    [Header("Set Dynamically")]
+    public GameObject lauchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    private Rigidbody projectileRigidbody;
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
+    void Awake()
+    {
+        S = this;
+
+        Transform launchPointTrans = transform.FindChild("LaunchPoint");
+        lauchPoint = launchPointTrans.gameObject;
+        lauchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+        lauchPoint.SetActive(true);
+
+    }
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+        lauchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        //玩家在鼠标光标悬停在弹弓上分时按下了鼠标左键
+        aimingMode = true;
+        //实例化一个弹丸
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //该示例的初始位置位于launchPoint处
+        projectile.transform.position = launchPos;
+        //设置当前的isKinematic属性
+        projectileRigidbody=projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //计算launchPos到mousePos3D两点间的坐标差
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //将mouseDelta坐标差限制在弹弓的球形碰撞器范围内
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if(mouseDelta.magnitude>maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        
+        //将projectile移动到新位置
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
+    }
+}
